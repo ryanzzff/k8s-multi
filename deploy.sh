c1a0f8d1b6f6eeffe2d40448d4dda079a01876ed
ryanzzff <ryanzzff@gmail.com>
2020-10-03 03:51:48 +0800

    add docker image tag with git SHA

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-docker build -t ryanzzff/docker-multi-containers-client -f ./client/Dockerfile ./client
-docker build -t ryanzzff/docker-multi-containers-server -f ./server/Dockerfile ./server
-docker build -t ryanzzff/docker-multi-containers-worker -f ./worker/Dockerfile ./worker
-docker push ryanzzff/docker-multi-containers-client
-docker push ryanzzff/docker-multi-containers-server
-docker push ryanzzff/docker-multi-containers-worker
+docker build -t ryanzzff/docker-multi-containers-client:latest -t ryanzzff/docker-multi-containers-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ryanzzff/docker-multi-containers-server:latest -t ryanzzff/docker-multi-containers-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ryanzzff/docker-multi-containers-worker:latest -t ryanzzff/docker-multi-containers-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ryanzzff/docker-multi-containers-client:latest
+docker push ryanzzff/docker-multi-containers-server:latest
+docker push ryanzzff/docker-multi-containers-worker:latest
+
+docker push ryanzzff/docker-multi-containers-client:$SHA
+docker push ryanzzff/docker-multi-containers-server:$SHA
+docker push ryanzzff/docker-multi-containers-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ryanzzff/docker-multi-containers-server
+kubectl set image deployments/server-deployment server=ryanzzff/docker-multi-containers-server:$SHA
+kubectl set image deployments/client-deployment client=ryanzzff/docker-multi-containers-client:$SHA
+kubectl set image deployments/worker-deployment worker=ryanzzff/docker-multi-containers-worker:$SHA
